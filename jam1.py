692a1abe450d7380819a03bf071eb12d12ee92fb
JosephRidge <josephridge18@gmail.com>
2026-06-17 20:00:41 +0300

    The Genesis

---
+++ jam1.py
@@ -0,0 +1,22 @@
+# this is a comment
+"""
+this is a multi-line string and can  be used as a multi-line comment
+
+"""
+
+"""
+PYTHON STREAMS:
+    - standard input
+    - standard output
+    - standard error
+"""
+
+user_name = "John Doe" # variable
+
+#  input => input()
+user_name = input("What is your name? ")
+# ouput => print()
+print(user_name)
+
+# errors => exceptions
+age # NameError: name 'age' is not defined => returns an erro since it has not been assigned a vlaue from hte word go
